bc179764d0274dfaeb50b493dccbe0ca32049bc7
rahulsharma123web <44479034+rahulsharma123web@users.noreply.github.com>
2019-10-16 22:40:38 +0530

    minor change

--- db/Db.go
+++ db/Db.go
@@ -10,7 +10,7 @@ import (
 func CreateDatabase() (*sql.DB, error) {
 	serverName := "localhost:3306"
 	user := "root"
-	password := "root@123"
+	password := "root@1234"
 	dbName := "demo"
 
 	connectionString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true&multiStatements=true", user, password, serverName, dbName)
